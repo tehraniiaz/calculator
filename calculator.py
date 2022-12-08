c9a7b9d68e8c5bf7a27f7dd8910b4c7cf1acbc7b
tehraniiaz <99446331+tehraniiaz@users.noreply.github.com>
2022-12-08 20:32:05 +0330

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+
+window=Tk()
+window.title("calculator")
+window.geometry("320x400")
+window.resizable(width="false",height="false")
+window.configure (bg="yellow")
+window.mainloop()
